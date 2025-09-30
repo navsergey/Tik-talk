f758bff4391d90d4f6e5d8e6ca1ed7b003aebc73
Sergey <mors195@yahoo.com>
2025-09-30 17:01:18 +0300

    API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProfileCard} from './common-ui/profile-card/profile-card';
+import {ProfileService} from './data/services/profile';
+import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCard,],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('tik-talk');
+
+  profileService = inject(ProfileService);
+  profiles:Profile[] = []
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe( val => {
+      this.profiles = val;
+    })
+  }
 }
